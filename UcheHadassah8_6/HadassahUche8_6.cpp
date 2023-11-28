8016a22e36ea93a67738ea5f71e3d17452fddf77
Hadassah Uche <uhadassah1@gmail.com>
2023-11-28 22:20:35 +0200

    Add project files.

---
+++ UcheHadassah8_6/HadassahUche8_6.cpp
@@ -0,0 +1,5 @@
+/*Use pointers to write a void function named remove() that deletes all occurrences of a 
+character from a C-string. The function should use two parameters: the C-string and the 
+character to be removed. For example if str contains the C-string Happy Holidays, the 
+function call remove(str, ‘H’) should change the string str to appy olidays. 
+Test your function.*/

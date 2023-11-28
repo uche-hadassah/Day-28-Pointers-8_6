92fa9896bbd2f0c50b77e231b8c74006519ca251
Hadassah Uche <uhadassah1@gmail.com>
2023-11-28 22:25:51 +0200

    I think this this how the main will generally look

--- UcheHadassah8_6/HadassahUche8_6.cpp
+++ UcheHadassah8_6/HadassahUche8_6.cpp
@@ -3,3 +3,22 @@ character from a C-string. The function should use two parameters: the C-string 
 character to be removed. For example if str contains the C-string Happy Holidays, the 
 function call remove(str, ‘H’) should change the string str to appy olidays. 
 Test your function.*/
+#include<iostream>
+#include<cstring>
+using namespace std;
+const int MAX = 100;
+void remove(char[], char);//Function prototype
+int main()
+{
+	char str[MAX];
+	char n;
+	cout << "Enter the string:";
+	cin.getline(str, MAX);
+	cout << "Enter the character you want to remove:";
+	cin >> n;
+	remove(str, n);
+}
+
+void remove(char[], char)
+{
+}

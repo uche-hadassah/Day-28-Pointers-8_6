e384a3d7dc754311801d9d2ae2976e8e2fb0f360
Hadassah Uche <uhadassah1@gmail.com>
2023-11-29 10:09:10 +0200

    Finally did it

--- UcheHadassah8_6/HadassahUche8_6.cpp
+++ UcheHadassah8_6/HadassahUche8_6.cpp
@@ -7,7 +7,7 @@ Test your function.*/
 #include<cstring>
 using namespace std;
 const int MAX = 100;
-void remove(char[], char);//Function prototype
+void remove(char*, const char);//Function prototype
 int main()
 {
 	char str[MAX];
@@ -17,8 +17,20 @@ int main()
 	cout << "Enter the character you want to remove:";
 	cin >> n;
 	remove(str, n);
+	cout << "the new string is " << str;
 }
 
-void remove(char[], char)
+void remove(char* Str, const char let)
 {
+	char* newPtr = Str;//Initialize a new pointer to the first position
+	while (*Str != '\0')//While the character at that position is not equal to the null character
+	{
+		if (*Str != let)//If the character at the position in the original string isn't the letter
+		{
+			*newPtr = *Str;//Copy the character to the new string if its not to be removed
+			newPtr++;//Increment the new pointer location to the next 
+		}
+		Str++;//Increment the pointer location to the next
+	}
+	*newPtr = '\0';//Terminate the new string
 }
